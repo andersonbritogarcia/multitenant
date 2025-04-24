e780cff7239e308ef88ed32a41e4a8520544d981
Anderson Brito <andersonbritogarcia@gmail.com>
2025-04-23 21:54:37 -0300

    feat: incluido cache no método de listagem de pets

--- app/src/main/java/tech/andersonbrito/app/pet/core/PetService.java
+++ app/src/main/java/tech/andersonbrito/app/pet/core/PetService.java
@@ -2,6 +2,7 @@ package tech.andersonbrito.app.pet.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tech.andersonbrito.app.pet.persistence.model.Pet;
@@ -27,6 +28,7 @@ public class PetService {
         return repository.save(new Pet(name, category));
     }
 
+    @Cacheable(value = "pets")
     @Transactional(readOnly = true)
     public List<Pet> getAllPets() {
         LOGGER.info("Getting all pets");
